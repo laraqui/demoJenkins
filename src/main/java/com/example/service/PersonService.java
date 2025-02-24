93afb1994d6ddbf2e4eb8bf35379989bb2b11509
laraqui <147731379+laraqui@users.noreply.github.com>
2025-02-24 23:33:28 +0000

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -46,6 +46,13 @@ public class PersonService {
 		p.setFirstName("webhook");
 		p.setLastName("testJenjinsJob");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("6");
+		p.setAge(30);
+		p.setFirstName("webhook2");
+		p.setLastName("testJenjinsJob2");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
